6c2997a86b08271d3bb0fc12bb6ffe49ba419534
Arkhipov Ivan <arkhipov.iv99@mail.ru>
2018-07-12 14:01:27 +0300

    third commit

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import numpy as np
-import math as m
+import math 
 import matplotlib.pyplot as plt
+import scipy
 
 def transform_12x1_to_3x4(data):
     transformed_data = []
@@ -19,41 +20,32 @@ def list_of_similars(data, n, m):
             biasi = data[i][:, 3]
             matrixj = data[j][:3, :3]
             biasj = data[j][:, 3]
-            if np.linalg.norm(biasi - biasj) < 2 \
-                    and np.dot(np.dot(matrixi, rotation_vector), np.dot(matrixj, rotation_vector)) / \
+            if np.linalg.norm(biasi - biasj) < n \
+                    and math.acos(np.dot(np.dot(matrixi, rotation_vector), np.dot(matrixj, rotation_vector)) / \
                     (np.linalg.norm(np.dot(matrixi, rotation_vector)) * \
-                     np.linalg.norm(np.dot(matrixj, rotation_vector))) > 0.9:
+                     np.linalg.norm(np.dot(matrixj, rotation_vector)))) < m:
                 similars.append((i, j))
     return similars
 
+#alg_result - list of (distance, picture1, picture2)
 def evaluation(alg_result, true_result):
-    auc = 0.0
     roc_x = []
     roc_y = []
-    min_distance = alg_result.min
-    max_distance = alg_result.max
-    t = min_distance
-    step = 0.1
     FP = 0
     TP = 0
     P = len(true_result)
-    N = m.factorial(alg_result.shape[0])/(m.factorial(2) * m.factorial(alg_result.shape[0] - 2)) - P
+    N = len(alg_result) - P
 
-    while t <= max_distance:
-        for i in range(alg_result.shape[0]):
-            for j in range(i, alg_result.shape[1]):
-                if alg_result[i, j] >= t:
-                    if true_result.count((i, j)) == 0:
-                        FP = FP + 1
-                    else:
-                        TP = TP + 1
+    alg_result.sort(key=lambda item: item[0])
+    for i in alg_result:
+        if true_result.count((i[1], i[2])) == 0:
+            FP = FP + 1
+        else:
+            TP = TP + 1
         roc_x.append(FP / float(N))
         roc_y.append(TP / float(P))
-        FP = 0
-        TP = 0
-        t = t + step
     plt.plot(roc_x, roc_y)
     plt.show()
-    return auc
+    return scipy.trapz(roc_y, roc_x)
 
 data = np.loadtxt("00.txt", dtype=np.float)
