03693a511535ed84d3f5dcfec9ed2f0cf6e5fc03
Arkhipov Ivan <arkhipov.iv99@mail.ru>
2018-08-04 15:59:14 +0300

    Siamese

---
+++ siamese.py
@@ -0,0 +1,34 @@
+import tensorflow as tf
+import tflearn
+
+
+class Siamese:
+
+    def __init__(self):
+        self.x1 = tflearn.input_data([None, 466616])
+        self.x2 = tflearn.input_data([None, 466616])
+
+        with tf.variable_scope("siamese") as scope:
+            self.network1 = self.network(self.x1)
+            scope.reuse_variables()
+            self.network2 = self.network(self.x2)
+
+        self.y_ = tf.placeholder(tf.float32, [None])
+
+    def network(self, x):
+        net = x
+        net = tflearn.fully_connected(net, 32)
+        net = tflearn.fully_connected(net, 32)
+        net = tflearn.fully_connected(net, 2)
+        return net
+
+    def loss(self):
+        margin = 8.0
+        y = self.y_
+        m = tf.constant(margin)
+        d = tf.sqrt(tf.reduce_sum(tf.pow(
+            tf.subtract(self.network1, self.network2), 2), 1), name="difference")
+        losses = tf.add(tf.multiply(tf.subtract(1.0, y), tf.pow(d, 2)),
+                        tf.multiply(y, tf.pow(tf.maximum(0.0, tf.subtract(m, d)), 2)))
+        loss = tf.reduce_mean(losses)
+        return loss
---
+++ truth_roc.py
@@ -0,0 +1,55 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def transform_12x1_to_3x4(data):
+    transformed_data = []
+
+    for i in data:
+        transformed_data.append(i.reshape(3, 4))
+    return transformed_data
+
+
+def matrix_of_similar(data, n, m):
+    similar = np.zeros((len(data), len(data)))
+
+    for i in range(len(data)):
+        for j in range(i, len(data)):
+            rotation_vector = np.array([0, 0, 1])
+            matrixi = data[i][:3, :3]
+            biasi = data[i][:, 3]
+            matrixj = data[j][:3, :3]
+            biasj = data[j][:, 3]
+            if np.linalg.norm(biasi - biasj) < n \
+                and np.arccos(np.dot(np.dot(matrixi, rotation_vector),
+                             np.dot(matrixj, rotation_vector)) /
+                      (np.linalg.norm(np.dot(matrixi, rotation_vector)) *
+                       np.linalg.norm(np.dot(matrixj, rotation_vector)))) < m:
+                similar[i][j] = 1
+    return similar
+
+
+def visualisation_roc(roc_x, roc_y):
+    plt.plot(roc_x, roc_y)
+    plt.show()
+
+
+def evaluation(alg_result, true_result):
+    alg_result_reshaped = np.reshape(alg_result,
+                                     (1, alg_result.shape[0] * alg_result.shape[0]))
+    true_result_reshaped = np.reshape(true_result,
+                                      (1, true_result.shape[0] * true_result.shape[0]))
+    result = np.reshape(np.dstack([alg_result_reshaped, true_result_reshaped]),
+                        (alg_result.size, 2))
+    result = np.asarray(sorted(result, key=lambda item: item[0]))
+    P = np.sum(true_result)
+    N = true_result.size - P
+    a = np.cumsum(result[:, 1])
+    b = np.arange(alg_result.size) + 1
+    roc_x = (b - a) / float(N)
+    roc_y = a / float(N)
+    visualisation_roc(roc_x, roc_y)
+    return np.sum(np.diff(np.append(roc_x, 1)) * roc_y)
+
+
+dataset = np.loadtxt("00.txt", dtype=np.float)
